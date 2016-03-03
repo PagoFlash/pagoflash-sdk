a68d0b274f08eb00d29d089672792ae911c5cba1
David <david_ks_18@hotmail.com>
2016-03-03 16:28:50 -0430

    Pagoflash python v2

--- pagoflash.api.client.php
+++ pagoflash.api.client.php
@@ -26,6 +26,9 @@ class apiPagoflash
   private $_url_punto_venta;
   private $_credenciales_pf;
   private $_codigo_error;
+  private  $_env='prod';
+  
+  static $GLOBAL_PARAMETERS=array(); 
 
   /**
    * Crea una nueva instancia de la clase
@@ -34,39 +37,57 @@ class apiPagoflash
    * recibida al momento de contratar el servicio de PagoFlash
    * @param string $p_key_secret Cadena que representa la clave de autenticación
    * recibida al momento de contratar el servicio de PagoFlash
-   * @param string $p_url_punto_venta URL del punto de venta virtual desde el cual
+   * @param string $p_url_punto_venta [deprecated] URL del punto de venta virtual desde el cual
    * se está realizando la llamada al servicio central de PagoFlash
    * @param boolean $p_modo_prueba Bandera que indica si las operaciones que
    * se realicen serán tratadas como pruebas de la aplicación
    */
-  function __construct($p_key_token, $p_key_secret, $p_url_punto_venta, $p_modo_prueba = FALSE)
+  function __construct($p_key_token, $p_key_secret, $p_url_punto_venta=null, $p_modo_prueba = FALSE)
   {
+    self::$GLOBAL_PARAMETERS=require dirname(__FILE__)."/pagoflash_parameters.php";
     $this->_codigo_error = 0;
     $this->_key_token = $p_key_token;
     $this->_key_secret = $p_key_secret;
     $this->_url_punto_venta = $p_url_punto_venta;
     $v_entorno = '';
     
+    
+    if($p_modo_prueba){
+        $this->_env="dev";
+    }
+    return $this;
+    
+    
+    
+    
+    
+    
+    
+    
     // se está utilizando la versión de prueba
     if($p_modo_prueba)
     {
-      $this->_dominio_base = 'http://api-test.pagoflash.com/';
+      $this->_dominio_base = 'http://127.0.0.1:8000/payment/generate-token';
+      //$this->_dominio_base = 'http://pagoflash/api.php';
       $v_entorno = self::ENTORNO_PRUEBA;
     }	
     // no se está utilizando la versión de prueba
     else
     {
-      $this->_dominio_base = 'https://api.pagoflash.com';  
+      $this->_dominio_base = 'http://127.0.0.1:8000/payment/generate-token';  
+      //$this->_dominio_base = 'http://pagoflash/api.php';
       $v_entorno = self::ENTORNO_PRODUCCION;
     }
-    
-    $v_entorno = self::ENTORNO_PRUEBA;
+
     // genera los parametros de autenticacion
-    $this->_credenciales_pf = "AUTH_KEY_SECRET={$this->_key_secret}"
+    $this->_credenciales_pf = 
+	"AUTH_KEY_SECRET={$this->_key_secret}"
       . "&AUTH_KEY_TOKEN={$this->_key_token}"
       . "&AUTH_SITE_URL={$this->_url_punto_venta}"
       . "&AUTH_ENV={$v_entorno}";
   }
+  
+ 
 
   /**
    * Indica si se está utilizando el modo de prueba de la API
@@ -128,25 +149,46 @@ class apiPagoflash
 
     // obtiene los parametros indicados como cabecera para la compra
     $v_cabecera_compra = isset($p_datos['cabecera_de_compra']) ? $p_datos['cabecera_de_compra'] : array();
+    foreach($v_cabecera_compra as $k=>$val){
+	$v_cabecera_compra[strtoupper($k)]=$val;
+    }
+    
+    $PagoFlashTokenBuilder= new PagoFlashTokenBuilder($this->_key_token,$this->_key_secret);
+    $PagoFlashTokenBuilder->setOrderInformation($p_datos['cabecera_de_compra']['pc_order_number'], $p_datos['cabecera_de_compra']['pc_amount'] );
+    foreach($p_datos['productos_items'] as $product_item){
+        $PagoFlashTokenBuilder->addProduct(
+                    $product_item['pr_name'],
+                    $product_item['pr_desc'],
+                    $product_item['pr_price'],
+                    $product_item['pr_qty'],
+                    $product_item['pr_img']
+                );
+    }
+    $response=$PagoFlashTokenBuilder->send(apiPagoflash::$GLOBAL_PARAMETERS[$this->_env]["domain"]);
+    return $response;
     
     // recorre los elementos indicados como cabecera
-    foreach ($v_cabecera_compra as $v_entrada => $v_valor)
+    $v_parametros_compra= http_build_query($v_cabecera_compra);
+    /*foreach ($v_cabecera_compra as $v_entrada => $v_valor)
     {
       // se indico un valor para el elemento
       if($v_valor != '')
       {
         $v_parametros_compra .= '&' . strtoupper($v_entrada) . '=' . urlencode($v_valor);
       }
-    }
+    }*/
 
     // obtiene los valores dados para los productos
-    $v_datos_productos = isset($p_datos['productos_items']) ? $p_datos['productos_items'] : array();
-    
+    $v_datos_productos["PRODUCTS"]=array();
+    $v_datos_productos["PRODUCTS"] = isset($p_datos['productos_items']) ? $p_datos['productos_items'] : array();
+    $v_datos_productos["PARAMS"]=array("url_ok_request"=>"HOLA.MUNDO");
     // inicializa el contador de productos
     $n = 0;
     
+     $v_productos_enviar .= '&'.http_build_query($v_datos_productos);
+//die($v_productos_enviar);
     // recorre los datos establecidos para cada producto
-    foreach ($v_datos_productos as $v_producto)
+    /*foreach ($v_datos_productos as $v_producto)
     {
       // recorre cada dato del producto para agregarlo a la trama a enviar
       foreach ($v_producto as $v_entrada => $v_valor)
@@ -160,19 +202,24 @@ class apiPagoflash
       
       // incrementa el contador de productos
       $n++;
-    }
+    }*/
 
     // genera la cadena de parámetros a enviar a la aplicación central de PagoFlash
-    $v_datos_post = $this->_credenciales_pf . $v_parametros_compra . $v_productos_enviar;
+    $key_to_encript=$v_cabecera_compra["PC_AMOUNT"].$v_cabecera_compra["PC_ORDER_NUMBER"].$this->_key_token;
+    $encripted_key=hash_hmac("sha256",$key_to_encript,$this->_key_secret);
+    $auth="AUTH_KEY_TOKEN={$this->_key_token}"
+      . "&AUTH_SITE_URL={$this->_url_punto_venta}"
+      . "&AUTH_ENV={$v_entorno}"
+      . "&AUTH_SIGNATURE={$encripted_key}";
+    $v_datos_post = $auth ."&". $v_parametros_compra . $v_productos_enviar;
     
-    //print $this->_dominio_base."?".$v_datos_post; exit();
+	//print $this->_dominio_base."?".$v_datos_post;
     // solicita la validación de la aplicación central de PagoFlash
     $v_respuesta = $this->generarSolicitudPOSTPagoFlash(
       $v_datos_post,
       $p_navegador
     );
-    
-    //print $this->_dominio_base."?".$v_datos_post." ";    exit();
+
     return $v_respuesta;
   }
 
@@ -233,6 +280,14 @@ class apiPagoflash
    * 
    * @return Un objeto mixto si la llamada fue exitosa o FALSE en caso contrario
    */
+
+
+
+
+  private function sendHTTPPOSTREquest(array $data, $format='json',array $auth=array()){
+      
+  }
+  
   private function generarSolicitudPOSTPagoFlash($p_datos, $p_navegador)
   {
     $this->_codigo_error = 0;
@@ -248,13 +303,13 @@ class apiPagoflash
     }
     
     // establece los parametors necesarios para la conexión
-    if(false == curl_setopt($v_curl, CURLOPT_VERBOSE, 1)){ $this->_codigo_error = self::ERROR_OPC_VERBOSE; }
-    if(false == curl_setopt($v_curl, CURLOPT_USERAGENT, $p_navegador)){ $this->_codigo_error = self::ERROR_OPC_USERAGENT; }
-    if(false == curl_setopt($v_curl, CURLOPT_SSL_VERIFYPEER, FALSE)){ $this->_codigo_error = self::ERROR_OPC_SSL; }
-    if(false == curl_setopt($v_curl, CURLOPT_TIMEOUT, 30)){ $this->_codigo_error = self::ERROR_OPC_TIMEOUT; }
-    if(false == curl_setopt($v_curl, CURLOPT_URL, $this->_dominio_base)){ $this->_codigo_error = self::ERROR_OPC_URL; }
-    if(false == curl_setopt($v_curl, CURLOPT_RETURNTRANSFER, 1)){ $this->_codigo_error = self::ERROR_OPC_RETURN; }
-    if(false == curl_setopt($v_curl, CURLOPT_POSTFIELDS, $p_datos)){ $this->_codigo_error = self::ERROR_OPC_POST; }
+    if(false == curl_setopt($v_curl, CURLOPT_VERBOSE, 1)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_VERBOSE; }
+    if(false == curl_setopt($v_curl, CURLOPT_USERAGENT, $p_navegador)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_USERAGENT; }
+    if(false == curl_setopt($v_curl, CURLOPT_SSL_VERIFYPEER, FALSE)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_SSL; }
+    if(false == curl_setopt($v_curl, CURLOPT_TIMEOUT, 30)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_TIMEOUT; }
+    if(false == curl_setopt($v_curl, CURLOPT_URL, $this->_dominio_base)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_URL; }
+    if(false == curl_setopt($v_curl, CURLOPT_RETURNTRANSFER, 1)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_RETURN; }
+    if(false == curl_setopt($v_curl, CURLOPT_POSTFIELDS, $p_datos)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_POST; }
 
     // envia la trama HTTP a la URL indicada
     $v_respuesta = curl_exec($v_curl);
@@ -279,8 +334,6 @@ class apiPagoflash
     $this->_dominio_base = $this->_dominio_base . "/transaction/valid-transaction.html";
     $v_datos_post = $this->_credenciales_pf ."&V_TK=" . $token_de_transaction;
     
-    //print $this->_dominio_base." ".$v_datos_post;
-    
     // solicita la validación de la aplicación central de PagoFlash
     $v_respuesta = $this->generarSolicitudPOSTPagoFlash(
       $v_datos_post,
@@ -290,3 +343,128 @@ class apiPagoflash
     return $v_respuesta;
   }
 }
+
+
+class PagoFlashTokenBuilder{
+    private $order_info=array();
+    private $products=array();
+    private $parameters=array();
+    
+    
+    public function __construct($key_token, $key_secret){
+        $this->authParams["KEY_TOKEN"]=$key_token;
+        $this->authParams["KEY_SECRET"]=$key_secret;
+    }
+    
+    public function setUrlOKRediect($url_ok_redirect){
+        $this->parameters["url_ok_redirect"]=$url_redirect;
+    }
+    /**
+     * URL a la que se hará la llamada HTTP una vez que el pago haya sido satisfactorio
+     * Usar este parámetro para hacer validaciones del pago de forma segura
+     * @param string $url_ok_request URL a la que se le hará un llamado una vez que el pago haya sido satisfactorio
+     * 
+     */
+    public function setUrlOKRequest($url_ok_request){
+        $this->parameters["url_ok_request"]=$url_redirect;
+    }
+    
+    
+    public function setOrderInformation($pc_order_number,$pc_amount){
+        $this->order_info=array(
+                "PC_ORDER_NUMBER"=>$pc_order_number,
+                "PC_AMOUNT"=>$pc_amount
+            );
+    }
+    public function addProduct($pr_name, $pr_desc, $pr_price, $pr_qty, $pr_img){
+        $this->products[]=array(
+                    'pr_name'    => $pr_name,        // Nombre.  127 char max.
+                    'pr_desc'    => $pr_desc, // Descripción .  Maximo 230 caracteres.
+                    'pr_price'   => $pr_price,                                         // Precio individual. Float, sin separadores de miles, utilizamos el punto (.) como separadores de Decimales. Máximo dos decimales
+                    'pr_qty'     => $pr_qty,                                         // Cantidad, Entero sin separadores de miles  
+                    'pr_img'     => $pr_img, // Dirección de imagen.  Debe ser una dirección (url) válida para la imagen.   
+        );
+    }
+    
+    
+    
+    public function send($domain){
+        $request=new PagoFlashHTTPRequest();
+        $key_to_encript=$this->order_info["PC_AMOUNT"].$this->order_info["PC_ORDER_NUMBER"].$this->authParams["KEY_TOKEN"];
+        $encripted_key=hash_hmac("sha256",$key_to_encript,$this->authParams["KEY_SECRET"]);
+        $dataToSend=$this->order_info;
+        $dataToSend["PRODUCTS"]=$this->products;
+        if(count($this->parameters)>0){
+            $dataToSend["PARAMETERS"]=$this->parameters;
+        }
+        $request->setRequestMethod("POST");
+        $request->setData($dataToSend);
+        $request->addHeader("X-Signature",$encripted_key);
+        $request->addHeader("X-Auth-Token", $this->authParams["KEY_TOKEN"] );
+        $url=$domain.'/payment/generate-token';
+        return $request->send($url);
+    }
+}
+
+class PagoFlashHTTPRequest{
+    private $headers=array();
+    private $data=array();
+    private $requestMethod='POST';
+    private $requestFormat="JSON";
+    
+    public function addHeader($key, $val){
+        $this->headers[$key]=$val;
+    }
+    public function setData(array $data){
+        $this->data=$data;
+    }
+    public function setRequestMethod($method){
+        $this->requestMethod=$method;
+    }
+    
+    public function getHeadersToCurl(){
+        $ret=array();
+        foreach ($this->headers as $k=>$val){
+            if(null==$val){
+                $ret[]=$k;
+            }else{
+                $ret[]=sprintf("%s: %s",$k,$val);
+            }
+        }
+        return $ret;
+    }
+    
+    public function send($url){
+        $dataToSend=$this->data;
+        $data_string = json_encode($dataToSend);
+        $this->_codigo_error = 0;
+    
+        $v_curl = curl_init();
+        // no se pudo inicializar la sesión
+        if(false == $v_curl)
+        {
+          $this->_codigo_error = apiPagoflash::ERROR_CURL_INIT;
+          return false;
+        }
+        if(false == curl_setopt($v_curl, CURLOPT_VERBOSE, 1)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_VERBOSE; }
+        if(false == curl_setopt($v_curl, CURLOPT_USERAGENT, 'pagoflash/SDK')){ $this->_codigo_error = apiPagoflash::ERROR_OPC_USERAGENT; }
+        if(false == curl_setopt($v_curl, CURLOPT_SSL_VERIFYPEER, FALSE)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_SSL; }
+        if(false == curl_setopt($v_curl, CURLOPT_TIMEOUT, 30)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_TIMEOUT; }
+        if(false == curl_setopt($v_curl, CURLOPT_URL, $url)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_URL; }
+        if(false == curl_setopt($v_curl, CURLOPT_RETURNTRANSFER, 1)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_RETURN; }
+        
+        if("JSON"==$this->requestFormat){
+            if(false == curl_setopt($v_curl, CURLOPT_POSTFIELDS, $data_string)){ $this->_codigo_error = apiPagoflash::ERROR_OPC_POST; }
+            $this->addHeader('Content-Type','application/json');
+            $this->addHeader('Content-Length', strlen($data_string));
+        }
+        var_dump($this->getHeadersToCurl());
+        curl_setopt($v_curl, CURLOPT_HTTPHEADER,$this->getHeadersToCurl());
+        $response=curl_exec($v_curl);
+        var_dump($response);
+        return $response;
+
+    }
+    
+}
+?>
