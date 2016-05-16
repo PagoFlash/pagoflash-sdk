0615dd11cb7d3d81e609029af1b27a94099528ed
David <david_ks_18@hotmail.com>
2016-05-16 15:16:43 -0430

    pagoflash paramenters

---
+++ pagoflash_parameters.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+  * @author Argenis Fontalvo [argenisfd@gmail.com]
+  * @version 0.0.1
+  * 
+  * IMPORTANTE: No modifique los valores de este archivo   
+  *
+  */
+
+return array(
+	'prod'=>array(
+		'domain'=>"127.0.0.1:8000"
+	),
+	'dev'=>array(
+		'domain'=>'127.0.0.1:8000'
+	)
+);
