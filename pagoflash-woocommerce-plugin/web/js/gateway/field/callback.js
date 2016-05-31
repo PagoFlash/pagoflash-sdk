983e64192541eeaaef3bad9ff7026bfd5c166031
David <david_ks_18@hotmail.com>
2016-05-31 10:29:02 -0430

    Folder deleted

--- pagoflash-woocommerce-plugin/web/js/gateway/field/callback.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function () {
-  jQuery(document.getElementById('callback-url-select-button')).on('click', function (p_event) {
-    jQuery(document.getElementById('callback-url')).select();
-  });
-});
